6eb7b5c0cbfbe10ae059b29dc3d7e6415034664f
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-04 21:55:36 +0100

    Create posts route in web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::post('/login', 'Auth\LoginController@login') -> name('login');
 
 Route::post('/register', 'Auth\RegisterController@register') -> name('register');
 Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
+
+Route::get('/posts', 'GuestController@posts') -> name('posts');
