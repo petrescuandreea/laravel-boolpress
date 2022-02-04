fc4a1bb32a840b15ce571b21a06aa9933ca61607
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-04 18:07:16 +0100

    Create home route in web

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth::routes();
 
-Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'GuestController@home') -> name('home');
