96f67481148169db0cadf872a84d73322072500f
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-04 21:56:06 +0100

    Add posts function in guest controller

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class GuestController extends Controller
 {
     public function home() {
 
         return view('pages.home');
+    }
+
+    public function posts() {
+
+        $posts = Post::all();
+
+        return view('pages.home', compact('posts'));
     }
 }
