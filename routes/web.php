28d71e3c63074bc41891d71a2825c3603b250dc0
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-04 18:39:08 +0100

    Create register/login and logout route in web

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'GuestController@home') -> name('home');
+
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
