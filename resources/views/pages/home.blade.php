c0bfda62b209b718977105ca17cb5a5a60890b62
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-05 13:55:20 +0100

    Fix: typo on btn-primary class in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,8 @@
             {{-- content for authenticated users  --}}
             <h1>{{ Auth::user() -> name }}</h1>
 
-            <a class="btn btn-prinary" href="{{ route('logout') }}">LOGOUT</a>
+            <a class="btn btn-primary" href="{{ route('logout') }}" role="button">LOGOUT</a>
+
 
             <section class="posts-container">
 
@@ -36,51 +37,51 @@
         @endauth
 
         @guest
-        {{-- registration form  --}}
-        <div id="registration">
-            <h2>Registration</h2>
-    
-            <form action="{{ route('register') }}" method="POST">
-    
-                @method("POST")
-                @csrf
-    
-                <label for="name">Name</label><br>
-                <input type="text" name="name" placeholder="Your Name"> <br>
-                <br>
-                <label for="email">E-mail</label><br>
-                <input type="email" name="email" placeholder="Your Email"> <br>
-                <br>
-                <label for="password">Password</label><br>
-                <input type="password" name="password" placeholder="Password"> <br>
-                <br>
-                <label for="password-confirm">Confirm password</label><br>
-                <input type="password" name="password_confirmation" placeholder="Repeat your password"> <br>
-                <br>
-                <br>
-                <input type="submit" value="REGISTER">
-            </form>
-        </div>
+            {{-- registration form  --}}
+            <div id="registration">
+                <h2>Registration</h2>
+        
+                <form action="{{ route('register') }}" method="POST">
+        
+                    @method("POST")
+                    @csrf
+        
+                    <label for="name">Name</label><br>
+                    <input type="text" name="name" placeholder="Your Name"> <br>
+                    <br>
+                    <label for="email">E-mail</label><br>
+                    <input type="email" name="email" placeholder="Your Email"> <br>
+                    <br>
+                    <label for="password">Password</label><br>
+                    <input type="password" name="password" placeholder="Password"> <br>
+                    <br>
+                    <label for="password-confirm">Confirm password</label><br>
+                    <input type="password" name="password_confirmation" placeholder="Repeat your password"> <br>
+                    <br>
+                    <br>
+                    <input type="submit" value="REGISTER">
+                </form>
+            </div>
 
-        {{-- Login form  --}}
-        <div id="log-in">
-            <h2>Login</h2>
-    
-            <form action="{{ route('login') }}" method="POST">
-    
-                @method("POST")
-                @csrf
-    
-                <label for="email"> Email</label><br>
-                <input type="email" name="email" placeholder="Your Email"> <br>
-                <br>
-                <label for="password">Password</label><br>
-                <input type="password" name="password" placeholder="Your Password"> <br>
-                <br>
-                <br>
-                <input type="submit" value="LOGIN">
-            </form>
-        </div>
+            {{-- Login form  --}}
+            <div id="log-in">
+                <h2>Login</h2>
+        
+                <form action="{{ route('login') }}" method="POST">
+        
+                    @method("POST")
+                    @csrf
+        
+                    <label for="email"> Email</label><br>
+                    <input type="email" name="email" placeholder="Your Email"> <br>
+                    <br>
+                    <label for="password">Password</label><br>
+                    <input type="password" name="password" placeholder="Your Password"> <br>
+                    <br>
+                    <br>
+                    <input type="submit" value="LOGIN">
+                </form>
+            </div>
         @endguest
     </section>
 
