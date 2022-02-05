4240e4d4f56894a1c1fcd335af44f379205091fe
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-05 14:57:37 +0100

    Add store function in guest controller

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -25,4 +25,25 @@ class GuestController extends Controller
 
         return view('pages.create');
     }
+
+    public function store(Request $request) {
+
+        // validation 
+        $data = $request -> validate([
+
+            'authorName' => 'required|string|max:255',
+            'authorPhoto' => 'required|string|url',
+            'postDate' => 'required|date',
+            'title' => 'required|string|max:255',
+            'subTitle' => 'nullable|string|max:255',
+            'postText' => 'required|max:15000',
+            'postImage' => 'nullable|string|url',
+        ]);
+
+        // create new post 
+        $post = Post::create($data);
+
+        // insert new element in posts 
+        return redirect() -> route('posts');
+    }
 }
