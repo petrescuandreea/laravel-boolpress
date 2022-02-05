d1e40a2d150ae5f313956026f856b28bc1ee5efa
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-05 14:58:25 +0100

    Add store route to form increate page

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,29 +4,32 @@
         Create new post
     </h1>
 
-    <form action="#">
+    <form action="{{ route('store') }}" method="POST">
 
-        <label for="authorName">Author Name:</label>
+        @method("POST")
+        @csrf
+
+        <label for="authorName">Author Name</label>
         <input type="text" name="authorName" placeholder="Author Name"><br>
 
-        <label for="authorPhoto">Author Photo:</label>
-        <input type="file" name="authorPhoto" placeholder="Author Photo"><br>
+        <label for="authorPhoto">Author Photo</label>
+        <input type="url" name="authorPhoto" placeholder="https://example.com"><br>
 
-        <label for="postDate">Post Date:</label>
+        <label for="postDate">Post Date</label>
         <input type="date" name="postDate" placeholder="Post Date"><br>
 
-        <label for="title">Title:</label>
+        <label for="title">Title</label>
         <input type="text" name="title" placeholder="Post Title"><br>
 
-        <label for="subTitle">Subtitle:</label>
+        <label for="subTitle">Subtitle</label>
         <input type="text" name="subTitle" placeholder="Post Subtitle"><br>
 
-        <label for="postText">Post Text:</label>
+        <label for="postText">Post Text</label>
         <textarea name="postText" rows="5" cols="30"></textarea> <br>
         
 
-        <label for="postImage">Post Image:</label>
-        <input type="file" name="postImage" placeholder="Post Photo"><br>
+        <label for="postImage">Post Image</label>
+        <input type="url" name="postImage" placeholder="https://example.com"><br>
 
         <input type="submit" value="Save new post">
     </form>
