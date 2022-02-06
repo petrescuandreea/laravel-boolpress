1ecc9e9033129b88e27864c22aed050d70452e32
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-06 16:25:02 +0100

    Add style to header with bootstrap classes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header>
+<header class="d-flex justify-content-start align-items-center pl-3" style="height:100px;">
     <img src="{{ asset('/storage/assets/boolpress.png') }}" alt="boolpress logo"> 
-    <h1>Boolpress</h1>
+    <h1 class="ml-3 mb-0">Boolpress</h1>
 </header>
