0a1cc9b015c20e61e8022c4e122f59780867bfe6
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-06 16:15:10 +0100

    Update forms in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.main-layout')
 @section('content')
-    
-    <section class="content">
-
+    <main>
         @auth
             {{-- content for authenticated users  --}}
-            <h1>{{ Auth::user() -> name }}</h1>
+            <h1 class=" text-center text-capitalize">Hi {{ Auth::user() -> name }} !</h1>
 
             <a class="btn btn-primary m-4" href="{{ route('logout') }}" role="button">LOGOUT</a>
 
@@ -34,56 +32,95 @@
             </section>
         @else
             {{-- content for guest users  --}}
-            <h1>You have to login or register to see our posts!</h1>
+            <h1 class="text-center" style="height:50px;">You have to login or register to see our posts or create one!</h1>
         @endauth
 
         @guest
-            {{-- registration form  --}}
-            <div id="registration">
-                <h2>Registration</h2>
-        
-                <form action="{{ route('register') }}" method="POST">
-        
-                    @method("POST")
-                    @csrf
-        
-                    <label for="name">Name</label><br>
-                    <input type="text" name="name" placeholder="Your Name"> <br>
-                    <br>
-                    <label for="email">E-mail</label><br>
-                    <input type="email" name="email" placeholder="Your Email"> <br>
-                    <br>
-                    <label for="password">Password</label><br>
-                    <input type="password" name="password" placeholder="Password"> <br>
-                    <br>
-                    <label for="password-confirm">Confirm password</label><br>
-                    <input type="password" name="password_confirmation" placeholder="Repeat your password"> <br>
-                    <br>
-                    <br>
-                    <input type="submit" value="REGISTER">
-                </form>
-            </div>
+        <section class="forms-container d-flex justify-content-center align-items-center" style="height:calc(100vh - 238px)">
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                {{-- registration form  --}}
+                <div id="registration" class="width:200px mr-5 p-4 bg-secondary border border-white rounded">
+                    <h2 class="text-dark text-center">Registration</h2>
+            
+                    <form action="{{ route('register') }}" method="POST">
+            
+                        @method("POST")
+                        @csrf
+                        
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 col-form-label text-dark">Name</label>
 
-            {{-- Login form  --}}
-            <div id="log-in">
-                <h2>Login</h2>
-        
-                <form action="{{ route('login') }}" method="POST">
-        
-                    @method("POST")
-                    @csrf
-        
-                    <label for="email"> Email</label><br>
-                    <input type="email" name="email" placeholder="Your Email"> <br>
-                    <br>
-                    <label for="password">Password</label><br>
-                    <input type="password" name="password" placeholder="Your Password"> <br>
-                    <br>
-                    <br>
-                    <input type="submit" value="LOGIN">
-                </form>
+                            <div class="col-md-6">
+                                <input type="text" name="name" placeholder="Your Name">
+                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="email" class="col-md-6 col-form-label text-dark">E-mail</label>
+
+                            <div class="col-md-6">
+                                <input type="email" name="email" placeholder="Your Email"> 
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password" class="col-md-4 col-form-label text-dark">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" name="password" placeholder="Password"> 
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-9 col-form-label text-dark">Confirm password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" name="password_confirmation" placeholder="Repeat your password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0 pt-2">
+                            <div class="col-md-6 offset-md-2">
+                                <input type="submit" class="btn btn-primary btn-lg" value="REGISTER">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+    
+                {{-- Login form  --}}
+                <div id="log-in" class="width:200px ml-5 p-4 bg-secondary border border-white rounded">
+                    <h2 class="text-dark text-center">Login Here</h2>
+            
+                    <form action="{{ route('login') }}" method="POST">
+            
+                        @method("POST")
+                        @csrf
+            
+                        <div class="form-group">
+                            <label for="email" class="col-md-6 col-form-label text-dark">E-mail</label>
+
+                            <div class="col-md-6">
+                                <input type="email" name="email" placeholder="Your Email"> 
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password" class="col-md-4 col-form-label text-dark">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" name="password" placeholder="Password"> 
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0 pt-2">
+                            <div class="col-md-6 offset-md-3">
+                                <input type="submit" class="btn btn-primary btn-lg" value="LOGIN">
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
+        </section>
         @endguest
-    </section>
-
+    </main>
 @endsection
