162801b32e6713e98ff0f4a03b071e83c238625c
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-06 20:15:15 +0100

    Add style to create new post form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,37 +1,76 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h1>
-        Create new post
-    </h1>
-
-    <form action="{{ route('store') }}" method="POST">
-
-        @method("POST")
-        @csrf
-
-        <label for="authorName">Author Name</label>
-        <input type="text" name="authorName" placeholder="Author Name"><br>
-
-        <label for="authorPhoto">Author Photo</label>
-        <input type="url" name="authorPhoto" placeholder="https://example.com"><br>
-
-        <label for="postDate">Post Date</label>
-        <input type="date" name="postDate" placeholder="Post Date"><br>
-
-        <label for="title">Title</label>
-        <input type="text" name="title" placeholder="Post Title"><br>
-
-        <label for="subTitle">Subtitle</label>
-        <input type="text" name="subTitle" placeholder="Post Subtitle"><br>
-
-        <label for="postText">Post Text</label>
-        <textarea name="postText" rows="5" cols="30"></textarea> <br>
-        
-
-        <label for="postImage">Post Image</label>
-        <input type="url" name="postImage" placeholder="https://example.com"><br>
-
-        <input type="submit" value="Save new post">
-    </form>
+    <section class="my-0 mx-auto text-center" style="width:75%;">
+        <h1>
+            Create new post
+        </h1>
     
+        <form action="{{ route('store') }}" method="POST">
+    
+            @method("POST")
+            @csrf
+    
+            <div class="form-group">
+                <label for="authorName" class="col-form-label text-light">Author Name</label>
+                
+                <div>
+                    <input type="text" name="authorName" placeholder="Author Name">
+                </div>
+            </div>
+    
+            <div class="form-group">
+                <label for="authorPhoto" class="col-form-label text-light">Author Photo</label>
+    
+                <div>
+                    <input type="url" name="authorPhoto" placeholder="https://example.com">
+                </div>
+            </div>
+    
+            <div class="form-group">
+                <label for="postDate" class="col-form-label text-light">Post Date</label>
+    
+                <div>
+                    <input type="date" name="postDate" placeholder="Post Date">
+                </div>
+            </div>
+    
+            <div class="form-group">
+                <label for="title" class="col-form-label text-light">Title</label>
+    
+                <div>
+                    <input type="text" name="title" placeholder="Post Title">
+                </div>
+            </div>
+    
+            <div class="form-group">
+                <label for="subTitle" class="col-form-label text-light">Subtitle</label>
+                
+                <div>
+                    <input type="text" name="subTitle" placeholder="Post Subtitle">
+                </div>
+            </div>
+    
+            <div class="form-group">
+                <label for="postText" class="col-form-label text-light">Post Text</label>
+    
+                <div>
+                    <textarea name="postText" rows="5" cols="30"></textarea>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="postImage" class="col-form-label text-light">Post Image</label>
+    
+                <div>
+                    <input type="url" name="postImage" placeholder="https://example.com">
+                </div>
+            </div>
+    
+            <div class="form-group row mb-0 pt-2">
+                <div class="col-lg-2 offset-lg-5">
+                    <input type="submit" class="btn btn-primary btn-lg" value="Save new post">
+                </div>
+            </div>
+        </form>
+    </section>
 @endsection
