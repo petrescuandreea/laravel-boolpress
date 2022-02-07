215583985d9c9921906ba97f20e3b05187c981b8
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-07 15:17:09 +0100

    Update create page

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main-layout')
 @section('content')
-    <section class="my-0 mx-auto text-center" style="width:75%;">
+    <section class="my-0 mx-auto text-center" style="width:75%; height:calc(100vh - 180px);">
         <h1>
             Create new post
         </h1>
@@ -9,30 +9,6 @@
     
             @method("POST")
             @csrf
-    
-            <div class="form-group">
-                <label for="authorName" class="col-form-label text-light">Author Name</label>
-                
-                <div>
-                    <input type="text" name="authorName" placeholder="Author Name">
-                </div>
-            </div>
-    
-            <div class="form-group">
-                <label for="authorPhoto" class="col-form-label text-light">Author Photo</label>
-    
-                <div>
-                    <input type="url" name="authorPhoto" placeholder="https://example.com">
-                </div>
-            </div>
-    
-            <div class="form-group">
-                <label for="postDate" class="col-form-label text-light">Post Date</label>
-    
-                <div>
-                    <input type="date" name="postDate" placeholder="Post Date">
-                </div>
-            </div>
     
             <div class="form-group">
                 <label for="title" class="col-form-label text-light">Title</label>
@@ -57,15 +33,7 @@
                     <textarea name="postText" rows="5" cols="30"></textarea>
                 </div>
             </div>
-            
-            <div class="form-group">
-                <label for="postImage" class="col-form-label text-light">Post Image</label>
-    
-                <div>
-                    <input type="url" name="postImage" placeholder="https://example.com">
-                </div>
-            </div>
-    
+
             <div class="form-group row mb-0 pt-2">
                 <div class="col-lg-2 offset-lg-5">
                     <input type="submit" class="btn btn-primary btn-lg" value="Save new post">
