a192b470a8e13c18c2d823487bfe2dfa75d6c3d7
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-07 15:19:41 +0100

    Update home, posts and store functions in guest controller

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use App\Post;
 
 class GuestController extends Controller
 {
     public function home() {
 
-        return view('pages.home');
+        return view('pages.index');
     }
 
     // show all posts 
     public function posts() {
 
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc') -> get();
 
         return view('pages.home', compact('posts'));
     }
@@ -31,15 +33,12 @@ class GuestController extends Controller
         // validation 
         $data = $request -> validate([
 
-            'authorName' => 'required|string|max:255',
-            'authorPhoto' => 'required|string|url',
-            'postDate' => 'required|date',
             'title' => 'required|string|max:255',
             'subTitle' => 'nullable|string|max:255',
-            'postText' => 'required|max:15000',
-            'postImage' => 'nullable|string|url',
+            'postText' => 'required|string|max:15000',
         ]);
 
+        $data['Author'] = Auth::user() ->name;
         // create new post 
         $post = Post::create($data);
 
