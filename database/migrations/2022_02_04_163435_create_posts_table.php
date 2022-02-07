64d992413120de772402cdcb2d23feaa8bdd9a81
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-07 21:03:09 +0100

    Add primary key in posts

--- database/migrations/2022_02_04_163435_create_posts_table.php
+++ database/migrations/2022_02_04_163435_create_posts_table.php
@@ -23,6 +23,9 @@ class CreatePostsTable extends Migration
             $table -> string('subTitle') -> nullable();
             $table -> text('postText');
 
+
+            $table -> bigInteger('category_id') -> unsigned();
+
             $table -> timestamps();
         });
     }
