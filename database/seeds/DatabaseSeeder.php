dec8ef506e1574a0b98ded634e84bcf3ddeb8eb6
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-07 21:07:07 +0100

    Add category seeder in db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostSeeder::class);
+        $this->call([
+
+            CategorySeeder::class,
+            PostSeeder::class,
+        ]);
     }
 }
