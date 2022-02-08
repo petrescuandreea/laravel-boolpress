e77cb9f659e02c48d43ff29224a43889b88b6ad2
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-08 15:13:11 +0100

    Create post_tag pivot table and add foreign keys

---
+++ database/migrations/2022_02_08_140159_create_post_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+
+            $table -> id();
+            
+            $table -> bigInteger('post_id') -> unsigned();
+            $table -> bigInteger('tag_id') -> unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
