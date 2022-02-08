c5991a0e9570042ec7ef5b97f78221c49df7053e
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-08 09:16:30 +0100

    Refactoring posts page

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -1,56 +1,47 @@
 @extends('layouts.main-layout')
 @section('content')
     <main>
-        @auth
-            {{-- content for authenticated users  --}}
-            <h1 class="text-center text-capitalize mb-4">Hi {{ Auth::user() -> name }} !</h1>
+        <h1 class="text-center text-capitalize mb-4">Hi {{ Auth::user() -> name }} !</h1>
 
-            <div class="d-flex justify-content-between" >
-                <div>
-                    <h3 class="pl-3">
-                        Here you can find our posts. If you want to create one don't hesitate, we will be happy to read it!
-                    </h3>
-                </div>
-
-                <div>
-                    <a class="btn btn-secondary mr-2" href="{{ route('create') }}" role="button">NEW POST</a>
-                    <a class="btn btn-primary mr-1" href="{{ route('logout') }}" role="button">LOGOUT</a>
-                </div>
+        <div class="d-flex justify-content-between" >
+            <div>
+                <h3 class="pl-3">
+                    Here you can find our posts. If you want to create one don't hesitate, we will be happy to read it!
+                </h3>
             </div>
 
-            <section class="d-flex flex-wrap justify-content-around py-5 text-info">
+            <div>
+                <a class="btn btn-secondary mr-2" href="{{ route('create') }}" role="button">NEW POST</a>
+                <a class="btn btn-primary mr-1" href="{{ route('logout') }}" role="button">LOGOUT</a>
+            </div>
+        </div>
 
-                @foreach ($posts as $post)
-                <div class="mb-5 py-5 border rounded border-top-0 text-center" style="width:calc(90% / 3);">
-                    <a href="" class="text-decoration-none">
-                        <h2 >
-                            Title: {{ $post -> title }}
-                        </h2>
-                        <h3>
-                            Subtitle: {{ $post -> subTitle ?? 'No subtitle' }}
-                        </h3>
-                        <span>
-                            Written by:( {{ $post -> authorName }} - {{ $post -> postDate }})
+        <section class="d-flex flex-wrap justify-content-around py-5 text-info">
+            @foreach ($posts as $post)
+            <div class="mb-5 py-5 border rounded border-top-0 text-center" style="width:calc(90% / 3);">
+                <a href="" class="text-decoration-none">
+                    <h2 >
+                        Title: {{ $post -> title }}
+                    </h2>
+                    <h3>
+                        Subtitle: {{ $post -> subTitle ?? 'No subtitle' }}
+                    </h3>
+                    <span>
+                        Written by:( {{ $post -> authorName }} - {{ $post -> postDate }})
+                    </span>
+
+                    <p>Category: {{ $post -> category -> name }}</p>
+                    <p>Descriprion: {{ $post -> category -> description }}</p>
+
+
+                    <div class="pt-3">
+                        <span class="d-block">
+                            Clik to read more ...
                         </span>
-
-                        <p>Category: {{ $post -> category -> name }}</p>
-                        <p>Descriprion: {{ $post -> category -> description }}</p>
-
-
-                        <div class="pt-3">
-                            <span class="d-block">
-                                Clik to read more ...
-                            </span>
-                        </div>
-                    </a>
-                </div>
-                @endforeach
-            </section>
-        @else
-            {{-- content for guest users  --}}
-            <h1 class="text-center" style="height:50px;">You have to login or register to see our posts or create one!</h1>
-        @endauth
-
-        
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </section>
     </main>
 @endsection
