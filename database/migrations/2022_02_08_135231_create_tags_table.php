56e2227f49089f022676a32fe0de96747d9a5746
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-08 16:26:41 +0100

    Update tags migration

--- database/migrations/2022_02_08_135231_create_tags_table.php
+++ database/migrations/2022_02_08_135231_create_tags_table.php
@@ -17,7 +17,7 @@ class CreateTagsTable extends Migration
 
             $table -> id();
 
-            $table -> string('name', 60) -> unique();
+            $table -> string('name');
 
             $table -> timestamps();
         });
